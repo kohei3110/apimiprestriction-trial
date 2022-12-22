41c678baf85fe06f332980069c9ea6454913750b
kohei3110 <kk.31108424@gmail.com>
2022-12-22 22:04:59 +0900

    関数呼び出し処理追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios, { AxiosResponse } from "axios";
 
 function App() {
+  const instance = axios.create({
+    baseURL:
+      "https://apim-gmoapp-trial-eastus2-001.azure-api.net/fn-gmoapp-trial-eastus2-001",
+    timeout: 1000,
+  });
+
+  try {
+    instance.get("/GetHeader");
+  } catch (e) {
+    console.log(e);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
